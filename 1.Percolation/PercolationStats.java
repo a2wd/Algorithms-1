c3b97ec098f306092ec15524e648625df933338a
a2wd <github@a2wd.com>
2015-06-20 01:00:17 +0100

    Added project readme

---
+++ 1.Percolation/PercolationStats.java
@@ -0,0 +1,33 @@
+/*
+ * PercolationStats.java
+ *
+ * A solution to the percolation problem via a monte carlo simulation
+ *
+ */
+
+public class PercolationStats
+{
+	public PercolationStats(int N, int T)
+	{
+	}
+
+	public double mean()
+	{
+	}
+
+	public double stddev()
+	{
+	}
+
+	public double confidenceLo()
+	{
+	}
+
+	public double confidenceHi()
+	{
+	}
+
+	public static void main(String[] args)
+	{
+	}
+}
