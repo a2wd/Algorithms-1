0b919879d0d64ba7515df50d1a69cd8c74326a3d
a2wd <github@a2wd.com>
2015-07-02 02:54:05 +0100

    Optimised classes for better time/memory handling

--- 2.Queueing/Subset.java
+++ 2.Queueing/Subset.java
@@ -1,45 +1,45 @@
 import java.util.Iterator;
 
 public class Subset {
-	private static Integer ParseInt(String s) {
-		try {
-			return Integer.parseInt(s);
-		}
-		catch(NumberFormatException e) {
-			return null;
-		}
-	}
 
-	private static void Usage() {
-		StdOut.println("Usage: Subset n\nSubset takes a list of values via StdIn and an integer n");
-		StdOut.println("Subset prints n of the values (where 0 <= n <= length)");
-	}
+  private static void usage() {
+    StdOut.println("Usage: Subset n\nSubset takes a list of values via StdIn and an integer n");
+    StdOut.println("Subset prints n of the values (where 0 <= n <= length)");
+  }
 
    public static void main(String[] args) {
-		 if(args.length < 1) {
-			 Usage();
-			 return;
-		 }
+     if (args.length < 1) {
+    	 usage();
+    	 return;
+     }
 
-		 Integer i = ParseInt(args[0]);
-		 if(i == null || i < 0) {
-			 Usage();
-			 return;
-		 }
+    int i;
+    try {
+    	i = Integer.parseInt(args[0]);
+    }
+    catch (NumberFormatException e) {
+    	usage();
+    	return;
+    }
 
-		 RandomizedQueue<String> rq = new RandomizedQueue<String>();
-		 while(!StdIn.isEmpty())
-			 rq.enqueue(StdIn.readString());
+     if (i < 0) {
+    	 usage();
+    	 return;
+     }
 
-		 if(i > rq.size()) {
-			 Usage();
-			 return;
-		 }
+     RandomizedQueue<String> rq = new RandomizedQueue<String>();
+     while (!StdIn.isEmpty())
+    	 rq.enqueue(StdIn.readString());
 
-		 Iterator<String> it = rq.iterator();
-		 int index = 0;
+     if (i > rq.size()) {
+    	 usage();
+    	 return;
+     }
 
-		 while(it.hasNext() && index++ < i)
-			 StdOut.println(it.next());
-	 }
+     Iterator<String> it = rq.iterator();
+     int index = 0;
+
+     while (it.hasNext() && index++ < i)
+    	 StdOut.println(it.next());
+   }
 }
